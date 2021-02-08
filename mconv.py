e572fec890d89b147fcc5e598d5e422607f85783
Tom Nikken <tom.nikken@student.ru.nl>
2021-02-08 23:13:49 +0100

    strip .yaml extension from conversation name

--- mconv.py
+++ mconv.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from mconv.create_functions import create_functions
@@ -9,12 +10,15 @@ if __name__ == "__main__":
         exit(-1)
 
     file_name = sys.argv[1]
+    if not file_name.endswith('.yaml'):
+        print(f'File name {file_name} should have .yaml extension')
+        exit(-1)
 
     with open(file_name, 'r') as infile:
         yaml = infile.read()
 
     conversation = parse_conversation(
-        conv_name=file_name,
+        conv_name=os.path.splitext(file_name)[0],
         yaml_conversation=yaml
     )
 
