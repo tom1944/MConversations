959a28872e7feb18ec09d45235eb33dabd05efd3
Tom Nikken <tom.nikken@student.ru.nl>
2021-02-13 17:07:04 +0100

    Extract LineFunctionCreator from FunctionCreator

--- mconv/create_functions.py
+++ mconv/create_functions.py
@@ -4,6 +4,7 @@ from typing import List
 
 from mconv.conversation import Conversation
 from mconv.function import Function
+from mconv.json_text import JSONText
 from mconv.line import Line
 
 
@@ -22,39 +23,9 @@ class FunctionCreator:
 
     def _make_line_functions(self) -> List[Function]:
         return [
-            self.make_line_function(line, index)
+            LineFunctionCreator(line, index, conversation_context=self.conversation).create_line()
             for index, line in enumerate(self.conversation.lines, start=1)
         ]
-
-    def make_line_function(self, line: Line, index: int) -> Function:
-        return Function(
-            name=self.conversation.name + '_' + str(index),
-            prefix=self.conversation.function_prefix,
-            commands=[f'tellraw @a {self._raw_json_text_for_line(line, index)}']
-        )
-
-    def _raw_json_text_for_line(self, line: Line, index: int) -> str:
-        speaker_name = self.conversation.speaker_name
-        total_lines = len(self.conversation.lines)
-
-        index_part = OrderedDict([
-            ("text", "(" + str(index) + "/" + str(total_lines) + ") "),
-            ("color", "gray"),
-            ("bold", True),
-        ])
-        speaker_part = OrderedDict([
-            ("text", speaker_name + ": "),
-            ("color", "yellow"),
-            ("bold", True),
-        ])
-        text_part = OrderedDict([
-            ("text", line.text),
-            ("color", "yellow"),
-        ])
-
-        json_text_as_json_object = ["", index_part, speaker_part, text_part]
-
-        return json.dumps(json_text_as_json_object)
 
     def _make_conversation_function(self, line_functions: List[Function]) -> Function:
         return Function(
@@ -81,3 +52,51 @@ def _make_schedule_command(func: Function, time: int) -> str:
         return f'function {func.get_identifier()}'
     else:
         return f'schedule function {func.get_identifier()} {time}s'
+
+
+class LineFunctionCreator:
+    def __init__(self, line: Line, index: int, conversation_context: Conversation):
+        self.line = line
+        self.index = index
+        self.conversation_context = conversation_context
+
+    def create_line(self) -> Function:
+        return Function(
+            name=self.conversation_context.name + '_' + str(self.index),
+            prefix=self.conversation_context.function_prefix,
+            commands=['tellraw @a ' + self._raw_json_text_for_line()]
+        )
+
+    def _raw_json_text_for_line(self) -> str:
+        index_part = self._make_json_text_for_index_part()
+        index_part['text'] += " "
+        speaker_part = self.make_json_text_for_speaker_part()
+        speaker_part['text'] += ": "
+        text_part = self._make_json_text_for_text_part()
+
+        json_text_as_json_object = ["", index_part, speaker_part, text_part]
+
+        return json.dumps(json_text_as_json_object)
+
+    def _make_json_text_for_index_part(self) -> JSONText:
+        total_lines = len(self.conversation_context.lines)
+        return OrderedDict([
+            ("text", "(" + str(self.index) + "/" + str(total_lines) + ")"),
+            ("color", "gray"),
+            ("bold", True),
+        ])
+
+    def make_json_text_for_speaker_part(self) -> JSONText:
+        speaker_name = self.conversation_context.speaker_name
+        speaker_part = OrderedDict([
+            ("text", speaker_name),
+            ("color", "yellow"),
+            ("bold", True),
+        ])
+        return speaker_part
+
+    def _make_json_text_for_text_part(self) -> JSONText:
+        return OrderedDict([
+            ("text", self.line.text),
+            ("color", "yellow"),
+        ])
---
+++ mconv/json_text.py
@@ -0,0 +1,4 @@
+from typing import Any
+
+# Json text is represented as a python object
+JSONText = Any
