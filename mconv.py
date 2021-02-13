dfbec3961ec48cff499851987453dd74b56d8fb9
Tom Nikken <tom.nikken@student.ru.nl>
2021-02-13 16:32:46 +0100

    Conversation name does not include path

--- mconv.py
+++ mconv.py
@@ -4,21 +4,27 @@ import sys
 from mconv.create_functions import create_functions
 from mconv.parse_conversation import parse_conversation
 
+
+def file_name_without_extension(path: str) -> str:
+    base = os.path.basename(path)
+    return os.path.splitext(base)[0]
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f'Usage: python {sys.argv[0]} <conversation_file>')
         exit(-1)
 
-    file_name = sys.argv[1]
-    if not file_name.endswith('.yaml'):
-        print(f'File name {file_name} should have .yaml extension')
+    path_name = sys.argv[1]
+    if not path_name.endswith('.yaml'):
+        print(f'File name {path_name} should have .yaml extension')
         exit(-1)
 
-    with open(file_name, 'r') as infile:
+    with open(path_name, 'r') as infile:
         yaml = infile.read()
 
     conversation = parse_conversation(
-        conv_name=os.path.splitext(file_name)[0],
+        conv_name=file_name_without_extension(path_name),
         yaml_conversation=yaml
     )
 
