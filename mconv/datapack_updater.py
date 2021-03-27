23dd0283170686c9e59c5d96d89da87f96cae05c
Tom Nikken <tom.nikken@student.ru.nl>
2021-03-27 16:14:01 +0100

    Add ConversationFileLocator

--- mconv/datapack_updater.py
+++ mconv/datapack_updater.py
@@ -1,5 +1,5 @@
 import os
-from typing import Iterator
+from typing import Iterator, List
 
 from mconv.conversation.conversation_context import ConversationContext
 from mconv.create_functions import create_functions
@@ -44,6 +44,47 @@ def subdirs(dirname: str) -> Iterator[str]:
             entry: os.DirEntry
             if entry.is_dir():
                 yield entry.name
+
+
+class ConversationFileLocator:
+    def __init__(self, datapack_path: str):
+        self._datapack_path = datapack_path
+
+    def locate_conversations_in_datapack(self) -> List[ConversationContext]:
+        datapack_path = self._datapack_path
+
+        if not os.path.isdir(datapack_path):
+            raise DatapackException(f"{datapack_path} is not a directory")
+
+        return list(self._locate_conversations_in_datapack())
+
+    def _locate_conversations_in_datapack(self) -> Iterator[ConversationContext]:
+        datapack_path = self._datapack_path
+        path_to_data_dir = os.path.join(datapack_path, DATAPACK_DATA_DIR)
+
+        if not os.path.isdir(path_to_data_dir):
+            raise DatapackException(f"{datapack_path} has no '{DATAPACK_DATA_DIR}' subdirectory")
+
+        for namespace in subdirs(path_to_data_dir):
+            path_to_functions_dir = os.path.join(path_to_data_dir, namespace, DATAPACK_FUNCTIONS_DIR)
+            yield from self._locate_conversations_in_functions_dir(namespace, path_to_functions_dir)
+
+    def _locate_conversations_in_functions_dir(self, namespace: str, path_to_functions_dir: str)\
+            -> Iterator[ConversationContext]:
+
+        if os.path.isdir(path_to_functions_dir):  # Check if the functions dir exists
+            for root, _, files in os.walk(path_to_functions_dir):
+                for file in files:
+                    if file.endswith(YAML_EXTENSION):
+                        conversation_name = file.removesuffix(YAML_EXTENSION)
+
+                        path_in_functions_dir = root.removeprefix(path_to_functions_dir).strip(os.sep)
+
+                        yield ConversationContext(
+                            namespace,
+                            path_in_functions_dir,
+                            conversation_name
+                        )
 
 
 class FunctionFilesCreator:
--- test/test_datapack_updater.py
+++ test/test_datapack_updater.py
@@ -1,7 +1,8 @@
 import os
 import unittest
 
-from mconv.datapack_updater import DatapackUpdater
+from mconv.conversation.conversation_context import ConversationContext
+from mconv.datapack_updater import DatapackUpdater, ConversationFileLocator
 
 
 class DatapackUpdaterTest(unittest.TestCase):
@@ -27,6 +28,20 @@ class DatapackUpdaterTest(unittest.TestCase):
                 os.remove(file)
             except FileNotFoundError:
                 pass
+
+    def test_conversation_file_locator(self):
+        conversation_file_locator = ConversationFileLocator('example-datapack')
+        expected_contexts = [
+            ConversationContext('mynamespace', '', 'conv'),
+            ConversationContext('mynamespace', 'mydir', 'conv-with-json-text')
+        ]
+
+        actual_contexts = conversation_file_locator.locate_conversations_in_datapack()
+
+        self.assertCountEqual(
+            expected_contexts,
+            actual_contexts,
+        )
 
 
 if __name__ == '__main__':
