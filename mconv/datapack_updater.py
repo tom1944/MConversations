3c69b38af536223b05a047749016ac745a9ee3c4
Tom Nikken <tom.nikken@student.ru.nl>
2021-03-27 17:46:55 +0100

    Use ConnversionFileLocator and cleanup DatapackUpdater

--- mconv/datapack_updater.py
+++ mconv/datapack_updater.py
@@ -3,6 +3,7 @@ from typing import Iterator, List
 
 from mconv.conversation.conversation_context import ConversationContext
 from mconv.create_functions import create_functions
+from mconv.minecraft_lang.function import Function
 from mconv.minecraft_lang.function_context import DATAPACK_DATA_DIR, DATAPACK_FUNCTIONS_DIR
 from mconv.parse_conversation import parse_conversation
 
@@ -14,36 +15,31 @@ class DatapackUpdater:
         self.datapack_path = datapack_path
 
     def update_conversations_in_datapack(self):
-        old_working_dir = os.getcwd()
-        os.chdir(self.datapack_path)
-        yaml_files = self._find_yaml_files_in_current_working_dir()
+        datapack_path = self.datapack_path
+        conversation_ctxs = ConversationFileLocator(datapack_path).locate_conversations_in_datapack()
 
-        for yaml_file in yaml_files:
-            FunctionFilesCreator(yaml_file).generate_mcfunction_files_for_yaml_file()
-            print(f'Successfully processed {yaml_file}')
-        os.chdir(old_working_dir)
+        for ctx in conversation_ctxs:
+            yaml = self._read_conversation_file(ctx)
 
-    def _find_yaml_files_in_current_working_dir(self) -> Iterator[str]:
-        if not os.path.isdir(DATAPACK_DATA_DIR):
-            raise DatapackException(f"{self.datapack_path} has no '{DATAPACK_DATA_DIR}' subdirectory")
+            conversation = parse_conversation(ctx, yaml)
+            functions = create_functions(conversation)
 
-        for namespace in subdirs(DATAPACK_DATA_DIR):
-            functions_dir = os.path.join(DATAPACK_DATA_DIR, namespace, DATAPACK_FUNCTIONS_DIR)
-            if os.path.isdir(functions_dir):
-                for root, dirs, files in os.walk(functions_dir):
-                    root: str
-                    for file in files:
-                        file: str
-                        if file.endswith(YAML_EXTENSION):
-                            yield os.path.join(root, file)
+            self._write_functions(functions)
 
+    def _read_conversation_file(self, ctx: ConversationContext) -> str:
+        path_to_yaml_file = os.path.join(self.datapack_path, ctx.as_path_in_datapack())
 
-def subdirs(dirname: str) -> Iterator[str]:
-    with os.scandir(dirname) as it:
-        for entry in it:
-            entry: os.DirEntry
-            if entry.is_dir():
-                yield entry.name
+        with open(path_to_yaml_file, 'r') as file:
+            yaml = file.read()
+
+        return yaml
+
+    def _write_functions(self, functions: List[Function]):
+        for function in functions:
+            file_name = os.path.join(self.datapack_path, function.context.as_filepath_in_datapack())
+
+            with open(file_name, 'w') as outfile:
+                outfile.write('\n'.join(function.commands))
 
 
 class ConversationFileLocator:
@@ -69,7 +65,8 @@ class ConversationFileLocator:
             path_to_functions_dir = os.path.join(path_to_data_dir, namespace, DATAPACK_FUNCTIONS_DIR)
             yield from self._locate_conversations_in_functions_dir(namespace, path_to_functions_dir)
 
-    def _locate_conversations_in_functions_dir(self, namespace: str, path_to_functions_dir: str)\
+    @staticmethod
+    def _locate_conversations_in_functions_dir(namespace: str, path_to_functions_dir: str)\
             -> Iterator[ConversationContext]:
 
         if os.path.isdir(path_to_functions_dir):  # Check if the functions dir exists
@@ -87,42 +84,12 @@ class ConversationFileLocator:
                         )
 
 
-class FunctionFilesCreator:
-    def __init__(self, yaml_filepath: str):
-        self.yaml_filepath = yaml_filepath.strip(os.sep)
-
-    def generate_mcfunction_files_for_yaml_file(self):
-        with open(self.yaml_filepath, 'r') as file:
-            yaml = file.read()
-
-        context = self._make_conversation_context()
-
-        conversation = parse_conversation(context, yaml)
-        functions = create_functions(conversation)
-
-        for function in functions:
-            function.export_to_file()
-
-    def _make_conversation_context(self) -> ConversationContext:
-        yaml_filepath = self.yaml_filepath
-        data_dir, namespace, functions_dir, filepath_in_functions_dir = yaml_filepath.split(os.sep, maxsplit=3)
-        if os.sep in filepath_in_functions_dir:
-            path, filename = filepath_in_functions_dir.rsplit(os.sep, maxsplit=1)
-        else:
-            path = ''
-            filename = filepath_in_functions_dir
-
-        if data_dir != DATAPACK_DATA_DIR or functions_dir != DATAPACK_FUNCTIONS_DIR\
-                or not filename.endswith(YAML_EXTENSION):
-            raise DatapackException(
-                f'{yaml_filepath} is not a path to a yaml conversation definition file inside a datapack'
-            )
-
-        return ConversationContext(
-            namespace,
-            path,
-            filename.rstrip(YAML_EXTENSION)
-        )
+def subdirs(dirname: str) -> Iterator[str]:
+    with os.scandir(dirname) as it:
+        for entry in it:
+            entry: os.DirEntry
+            if entry.is_dir():
+                yield entry.name
 
 
 class DatapackException(Exception):
--- mconv/minecraft_lang/function.py
+++ mconv/minecraft_lang/function.py
@@ -4,15 +4,9 @@ from mconv.minecraft_lang.function_context import FunctionContext
 
 
 class Function:
-    def __init__(self, commands: List[str], function_context: FunctionContext):
+    def __init__(self, commands: List[str], function_context: FunctionContext):  # TODO van deze een meer een NamedTuple maken
         self.commands = commands
         self.context = function_context
-
-    def export_to_file(self):
-        file_name = self.context.as_filepath_in_datapack()
-
-        with open(file_name, 'w') as outfile:
-            outfile.write('\n'.join(self.commands))
 
     def get_qualified_function_name(self) -> str:
         return self.context.as_qualified_function_name()
