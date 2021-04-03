ef3b156c3257806a199b38d85736685e99ee36de
Tom Nikken <tom.nikken@student.ru.nl>
2021-04-03 17:32:52 +0200

    Create and test test fixture objects

--- test/fixture/fixture.py
+++ test/fixture/fixture.py
@@ -1,6 +1,6 @@
 import os
 from collections import OrderedDict
-from typing import List, Tuple
+from typing import List, Tuple, NamedTuple
 
 from mconv.conversation.conversation import Conversation
 from mconv.conversation.conversation_context import ConversationContext
@@ -9,20 +9,33 @@ from mconv.minecraft_lang.function_context import FunctionContext
 from mconv.conversation.line import Line
 
 
-def make_simple_conversation_yaml() -> Tuple[ConversationContext, str]:
-    with open(os.sep.join(['example-datapack', 'data', 'mynamespace', 'functions', 'conv.yaml'])) as file:
+class MConvTestFixture(NamedTuple):
+    file_path: str
+    yaml: str
+
+    conv_ctx: ConversationContext
+    conversation: Conversation
+
+    functions: List[Function]
+
+
+def make_conv_fixtures() -> List[MConvTestFixture]:
+    return [
+        make_simple_conv_fixture(),
+        make_conv_with_json_text_fixture(),
+    ]
+
+
+def make_simple_conv_fixture() -> MConvTestFixture:
+    file_path = os.sep.join(['example-datapack', 'data', 'mynamespace', 'functions', 'conv.yaml'])
+
+    with open(file_path) as file:
         yaml = file.read()
 
-    return make_conv_ctx_simple_conversation_yaml(), yaml
+    conv_ctx = ConversationContext('mynamespace', '', 'conv')
 
-
-def make_conv_ctx_simple_conversation_yaml() -> ConversationContext:
-    return ConversationContext('mynamespace', '', 'conv')
-
-
-def make_simple_conversation_object() -> Conversation:
-    return Conversation(
-        make_conv_ctx_simple_conversation_yaml(),
+    conversation = Conversation(
+        conv_ctx,
         'Erik',
         [
             Line('Hello!', speak_time=2),
@@ -31,23 +44,16 @@ def make_simple_conversation_object() -> Conversation:
         ]
     )
 
+    fcc = _FunctionContextCreator('mynamespace', '')
 
-def make_simple_conversation_functions() -> List[Function]:
-    def _make_context_from_function_name(function_name: str) -> FunctionContext:
-        return FunctionContext(
-            namespace='mynamespace',
-            path_in_functions_dir='',
-            function_name=function_name
-        )
-
-    return [
+    functions = [
         Function(
             commands=[
                 'function mynamespace:conv_1',
                 'schedule function mynamespace:conv_2 2s',
                 'schedule function mynamespace:conv_3 5s',
             ],
-            function_context=_make_context_from_function_name('conv')
+            function_context=fcc.make_function_context('conv')
         ),
         Function(
             commands=[
@@ -57,7 +63,7 @@ def make_simple_conversation_functions() -> List[Function]:
                 '{"text": "Hello!", "color": "yellow"}'
                 ']'
             ],
-            function_context=_make_context_from_function_name('conv_1')
+            function_context=fcc.make_function_context('conv_1')
         ),
         Function(
             commands=[
@@ -67,7 +73,7 @@ def make_simple_conversation_functions() -> List[Function]:
                 '{"text": "This is a very long text that requires 3 seconds of reading time", "color": "yellow"}'
                 ']'
             ],
-            function_context=_make_context_from_function_name('conv_2')
+            function_context=fcc.make_function_context('conv_2')
         ),
         Function(
             commands=[
@@ -77,25 +83,40 @@ def make_simple_conversation_functions() -> List[Function]:
                 '{"text": "This is the end...", "color": "yellow"}'
                 ']'
             ],
-            function_context=_make_context_from_function_name('conv_3')
+            function_context=fcc.make_function_context('conv_3')
         ),
     ]
 
+    return MConvTestFixture(file_path, yaml, conv_ctx, conversation, functions)
 
-def make_conversation_using_json_text_yaml() -> Tuple[ConversationContext, str]:
-    with open(os.sep.join(['example-datapack', 'data', 'mynamespace', 'functions', 'mydir', 'conv-with-json-text.yaml'])) as file:
+
+def make_simple_conversation_yaml() -> Tuple[ConversationContext, str]:
+    simple_conv_fixture = make_simple_conv_fixture()
+    return simple_conv_fixture.conv_ctx, simple_conv_fixture.yaml
+
+
+def make_conv_ctx_simple_conversation_yaml() -> ConversationContext:
+    return make_simple_conv_fixture().conv_ctx
+
+
+def make_simple_conversation_object() -> Conversation:
+    return make_simple_conv_fixture().conversation
+
+
+def make_simple_conversation_functions() -> List[Function]:
+    return make_simple_conv_fixture().functions
+
+
+def make_conv_with_json_text_fixture() -> MConvTestFixture:
+    file_path = os.sep.join(['example-datapack', 'data', 'mynamespace', 'functions', 'mydir', 'conv-with-json-text.yaml'])
+
+    with open(file_path) as file:
         yaml = file.read()
 
-    return make_conv_ctx_using_json_text_yaml(), yaml
+    conv_ctx = ConversationContext('mynamespace', 'mydir', 'conv-with-json-text')
 
-
-def make_conv_ctx_using_json_text_yaml() -> ConversationContext:
-    return ConversationContext('mynamespace', 'mydir', 'conv-with-json-text')
-
-
-def make_conversation_using_json_text_object() -> Conversation:
-    return Conversation(
-        make_conv_ctx_using_json_text_yaml(),
+    conversation = Conversation(
+        conv_ctx,
         speaker_name=OrderedDict([
             ("text", "Erik"),
             ("color", "red"),
@@ -111,19 +132,12 @@ def make_conversation_using_json_text_object() -> Conversation:
         ]
     )
 
+    fcc = _FunctionContextCreator('mynamespace', 'mydir')
 
-def make_conversation_using_json_text_functions() -> List[Function]:
-    def _make_context_from_function_name(function_name: str) -> FunctionContext:
-        return FunctionContext(
-            namespace='mynamespace',
-            path_in_functions_dir='mydir',
-            function_name=function_name
-        )
-
-    return [
+    functions = [
         Function(
             commands=['function mynamespace:mydir/conv-with-json-text_1'],
-            function_context=_make_context_from_function_name('conv-with-json-text')
+            function_context=fcc.make_function_context('conv-with-json-text')
         ),
         Function(
             commands=[
@@ -133,6 +147,38 @@ def make_conversation_using_json_text_functions() -> List[Function]:
                 '{"text": "Hello!", "color": "blue"}'
                 ']'
             ],
-            function_context=_make_context_from_function_name('conv-with-json-text_1')
+            function_context=fcc.make_function_context('conv-with-json-text_1')
         ),
     ]
+
+    return MConvTestFixture(file_path, yaml, conv_ctx, conversation, functions)
+
+
+def make_conversation_using_json_text_yaml() -> Tuple[ConversationContext, str]:
+    conversation_using_json_text = make_conv_with_json_text_fixture()
+    return conversation_using_json_text.conv_ctx, conversation_using_json_text.yaml
+
+
+def make_conv_ctx_using_json_text_yaml() -> ConversationContext:
+    return make_conv_with_json_text_fixture().conv_ctx
+
+
+def make_conversation_using_json_text_object() -> Conversation:
+    return make_conv_with_json_text_fixture().conversation
+
+
+def make_conversation_using_json_text_functions() -> List[Function]:
+    return make_conv_with_json_text_fixture().functions
+
+
+class _FunctionContextCreator:
+    def __init__(self, namespace: str, path_in_functions_dir: str):
+        self.namespace = namespace
+        self.path_in_functions_dir = path_in_functions_dir
+
+    def make_function_context(self, function_name: str) -> FunctionContext:
+        return FunctionContext(
+            self.namespace,
+            self.path_in_functions_dir,
+            function_name
+        )
