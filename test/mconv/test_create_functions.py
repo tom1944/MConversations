919b7d9c844301a254a031d2b86fe9ec5c6ac254
Tom Nikken <tom.nikken@student.ru.nl>
2021-04-04 13:12:47 +0200

    Add test for function on beginning of conv

--- test/mconv/test_create_functions.py
+++ test/mconv/test_create_functions.py
@@ -1,6 +1,11 @@
 import unittest
 
+from mconv.conversation.conversation import Conversation
+from mconv.conversation.conversation_context import ConversationContext
+from mconv.conversation.line import FunctionLine, TextLine
 from mconv.create_functions import create_functions
+from mconv.minecraft_lang.function import Function
+from mconv.minecraft_lang.function_context import FunctionContext
 from test.fixture.test_case_with_fixtures import TestCaseWithFixture
 
 
@@ -10,6 +15,41 @@ class CreateFunctionsTest(TestCaseWithFixture):
             with self.subTest(msg=conv_fixture.conv_ctx.name):
                 actual_functions = create_functions(conv_fixture.conversation)
                 self.assertCountEqual(conv_fixture.functions, actual_functions)
+
+    def test_function_on_beginning_of_conv(self):
+        conv_ctx = ConversationContext('namespace', '', 'conv-name')
+        conversation = Conversation(
+            conv_ctx,
+            'Erik',
+            [
+                FunctionLine('other-namespace:reward-function'),
+                TextLine('You got reward!', speak_time=5)
+            ]
+        )
+
+        expected_functions = [
+            Function(
+                commands=[
+                    'function other-namespace:reward-function',
+                    'function namespace:conv-name_1',
+                ],
+                function_context=FunctionContext('namespace', '', 'conv-name')
+            ),
+            Function(
+                commands=[
+                    'tellraw @a ["", '
+                    '{"text": "(1/1) ", "color": "gray", "bold": true}, '
+                    '{"text": "Erik: ", "color": "yellow", "bold": true}, '
+                    '{"text": "You got reward!", "color": "yellow"}'
+                    ']'
+                ],
+                function_context=FunctionContext('namespace', '', 'conv-name_1')
+            )
+        ]
+
+        actual_functions = create_functions(conversation)
+
+        self.assertCountEqual(expected_functions, actual_functions)
 
 
 if __name__ == '__main__':
